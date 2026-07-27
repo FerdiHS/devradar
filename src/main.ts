8b2e6ba05fc921a0d333bd6c1148733a3e15815c
FerdiHS <ferdi.fhs.fhs@gmail.com>
2026-07-27 23:06:01 +0700

    refactor: trim bootstrap and docs

--- src/main.ts
+++ src/main.ts
@@ -13,8 +13,6 @@ export default class DevRadarPlugin extends Plugin {
 		this.addSettingTab(new DevRadarSettingTab(this.app, this));
 	}
 
-	onunload() {}
-
 	async loadSettings() {
 		this.settings = Object.assign(
 			{},
