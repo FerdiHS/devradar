8368df36f3fb92ee7fe7d842081eec636550dcfa
FerdiHS <ferdi.fhs.fhs@gmail.com>
2026-07-28 13:47:40 +0700

    refactor: simplify empty settings tab

--- src/settings.ts
+++ src/settings.ts
@@ -1,22 +1,17 @@
-import { App, PluginSettingTab, Setting } from 'obsidian';
-import DevRadarPlugin from './main';
+import { PluginSettingTab } from 'obsidian';
 
 export type DevRadarSettings = Record<string, never>;
 
 export const DEFAULT_SETTINGS: DevRadarSettings = {};
 
 export class DevRadarSettingTab extends PluginSettingTab {
-	constructor(app: App, plugin: DevRadarPlugin) {
-		super(app, plugin);
-	}
-
 	display(): void {
 		const { containerEl } = this;
 
 		containerEl.empty();
 
-		new Setting(containerEl)
-			.setName('No settings yet')
-			.setDesc('There are no configurable settings yet.');
+		containerEl.createEl('p', {
+			text: 'There are no configurable settings yet.',
+		});
 	}
 }
