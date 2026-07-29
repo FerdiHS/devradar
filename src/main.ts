be48e2d27d5bbeab726fc58b6797d4fa1903acce
FerdiHS <ferdi.fhs.fhs@gmail.com>
2026-07-29 16:14:53 +0700

    refactor: remove dead settings layer

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,3 @@
 import { Plugin } from 'obsidian';
-import { createSettings, type DevRadarSettings } from './settings-data';
 
-export default class DevRadarPlugin extends Plugin {
-	settings!: DevRadarSettings;
-
-	async onload() {
-		await this.loadSettings();
-	}
-
-	async loadSettings() {
-		this.settings = createSettings(
-			(await this.loadData()) as Partial<DevRadarSettings>,
-		);
-	}
-}
+export default class DevRadarPlugin extends Plugin {}
--- src/settings-data.ts
+++
@@ -1,7 +0,0 @@
-export type DevRadarSettings = Record<string, never>;
-
-export function createSettings(
-	raw: Partial<DevRadarSettings> | null | undefined,
-): DevRadarSettings {
-	return { ...(raw ?? {}) } as DevRadarSettings;
-}
